1392842ff150e40d493363ff60b99ddd4bd1fb64
Ramen <zwang207@students.cs.ubc.ca>
2023-10-12 21:20:31 -0700

    To come back to if needed

--- src/main/ui/Game.java
+++ src/main/ui/Game.java
@@ -1,7 +1,9 @@
 package ui;
 
 import model.Cookie;
-//import model.CookieHelper;
+
+import java.util.List;
+import java.util.ArrayList;
 
 import java.util.Scanner;
 import java.util.concurrent.Executors;
@@ -14,18 +16,18 @@ import java.util.concurrent.TimeUnit;
 // Game
 public class Game {
     Cookie cookie;
-//    CookieHelper cookieHelper;
+    List<Cookie> cookieList;
 
     public Game() {
+        cookieList = new ArrayList<>();
+        System.out.println("Welcome to Cookie Clicker!");
         makeCookie();
         playGame();
     }
 
     public void playGame() {
         Boolean isRunning = true;
-        System.out.println("Here are your options:\n\tSpacebar - add one cookie\n\ta - obtain information about your "
-                + "current cookies\n\ts - purchase a cookie helper that gives you cookies (costs 5 cookies)\n\tz - "
-                + "start making different cookies\n\tx - quit");
+        startMessage();
         Scanner input = new Scanner(System.in);
         while (isRunning) {
             String command = input.nextLine().toLowerCase();
@@ -36,6 +38,7 @@ public class Game {
             } else if (command.equals("s")) {
                 cookieHelper();
             } else if (command.equals("z")) {
+                report();
                 makeCookie();
             } else if (command.equals("x")) {
                 isRunning = false;
@@ -43,7 +46,7 @@ public class Game {
                 System.out.println("Invalid input, please try again.");
             }
         }
-        System.out.println("Thanks for playing! You ended with " + cookie.getNumCookies() + " cookies!");
+        conclude();
     }
 
     public void cookieHelper() {
@@ -59,14 +62,37 @@ public class Game {
         }
     }
 
+    public void report() {
+        System.out.println("You ended up with " + cookie.getNumCookies() + " cookies!"
+                + " It is worth a total of $" + cookie.getCost() + ". Data has been added to the list.");
+        cookieList.add(cookie);
+    }
 
     public void makeCookie() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Cookie Clicker! What kind of cookie would you like to make? ");
+        System.out.println("What kind of cookie would you like to make? ");
         String name = input.nextLine();
         System.out.println("How much do you want to sell each cookie for (in $)? ");
-        int value =  Integer.parseInt(input.nextLine());
+        int value = Integer.parseInt(input.nextLine());
         cookie = new Cookie(name, value);
     }
 
+    public void startMessage() {
+        System.out.println("Here are your options:\n\tSpacebar - add one cookie\n\ta - obtain information about your "
+                + "current cookies\n\ts - purchase a cookie helper that gives you cookies (costs 5 cookies)\n\tz - "
+                + "start making different cookies\n\tx - quit");
+    }
+
+    public void conclude() {
+        int totalCost = 0;
+        System.out.println("Thanks for playing! You ended with " + cookieList.size() + " different collections of"
+                + "cookies:");
+        for (int num = 0; num < cookieList.size(); num++) {
+            cookie = cookieList.get(num);
+            System.out.println("You had " + cookie.getNumCookies() + cookie.getName() + " cookies. They were worth $"
+                    + cookie.getCost() + "!");
+            totalCost += cookie.getCost();
+        }
+        System.out.println("The total value of all your cookies is $" + totalCost);
+    }
 }
